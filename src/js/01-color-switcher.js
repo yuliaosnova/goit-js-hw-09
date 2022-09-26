c5eee40736a48ab60b77f14db32218449e043871
Yulia Osnova <yulia.osnova@gmail.com>
2022-09-26 20:43:19 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const bodyEl = document.querySelector("body");
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+let timerId = null;
 
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+startBtn.addEventListener("click", () => {
+
+	timerId = setInterval(() => {
+		console.log("bleat");
+	const color = getRandomHexColor();
+	bodyEl.style.backgroundColor = color;
+	}, 1000);
+});
+ 
+ stopBtn.addEventListener("click", () => {
+	clearInterval(timerId)
+});
